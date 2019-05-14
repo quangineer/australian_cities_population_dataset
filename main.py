f6b1be857bef5aadc10fc511141315635e8cf3b6
Quan Nguyen <quangineer@gmail.com>
2019-05-13 17:24:16 -0700

    updates

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pandas as pd 
-import os
 import matplotlib.pyplot as plt 
+import matplotlib
 import squarify 
 
-cityfile = "AUS_state.csv"
-data = pd.read_csv(cityfile, usecols=["State/Territory","June 2017[2]"])
-print (data)
-list_of_cities = data["State/Territory"].tolist()
-list_of_populations = data["June 2017[2]"].tolist()
+file_aus = "AUS_state.csv"
+data = pd.read_csv(file_aus)
 
+data_2017 = data["June 2017[2]"]
+
+#Making a list of 2017 populations cities by stripping off all commas:
+Population_2017 = []
+for i in data_2017:
+    a = int(i.replace(",",""))
+    Population_2017.append(a)
+
+#New dataframe, containing only population of cities with more than 0 person living in:
+dataPopulations = data["Population_2017">0]
+print (dataPopulations)
+
+# #Utilize matplotlib to scale our population between the min and the max, then assign this scale to our values.
+# norm = matplotlib.colors.Normalize(vmin=min())
